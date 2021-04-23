157c1c83b284325acf093507efbe23308df927aa
CHRIS_LEE <chrismlee87@gmail.com>
2021-04-23 13:25:14 -0700

    Users can view a list of posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,6 +1,16 @@
 const Post = require('../models/post');
 
 module.exports = (app) => {
+  // GET POSTS-INDEX
+  app.get('/', (req, res) => {
+    Post.find({}).lean()
+      .then(posts => {
+        res.render('posts-index', { posts });
+      })
+      .catch(err => {
+        console.log(err.message);
+      })
+  })
 
   // CREATE
   app.post('/posts/new', (req, res) => {
--- data/reddit-db.js
+++ data/reddit-db.js
@@ -6,8 +6,7 @@ const url = "mongodb://localhost/reddit-db";
 mongoose.Promise = global.Promise;
 mongoose.connect(
   url,
-  { useUnifiedTopology: true },
-  { useNewUrlParser: true },
+  { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, },
   function(err, db) {
     assert.equal(null, err);
     console.log("Connected successfully to database");
