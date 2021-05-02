32074fadf576f94a06a0cd1990c2568ecf09ef19
CHRIS_LEE <chrismlee87@gmail.com>
2021-05-01 22:45:52 -0700

    Users must be logged in to submit a post

--- controllers/posts.js
+++ controllers/posts.js
@@ -3,9 +3,11 @@ const Post = require('../models/post');
 module.exports = (app) => {
   // GET POSTS-INDEX
   app.get('/', (req, res) => {
+    var currentUser = req.user;
+
     Post.find({}).lean()
       .then(posts => {
-        res.render('posts-index', { posts });
+        res.render('posts-index', { posts, currentUser });
       })
       .catch(err => {
         console.log(err.message);
@@ -15,18 +17,23 @@ module.exports = (app) => {
   // CREATE
   app.post('/posts/new', (req, res) => {
     // INSTANTIATE INSTANCE OF POST MODEL
-    const post = new Post(req.body);
+    if (req.user) {
+      var post = new Post(req.body);
     // SAVE INSTANCE OF POST MODEL TO DB
-    post.save((err, post) => {
-      // REDIRECT TO THE ROOT
-      return res.redirect('/');
-    })
+      post.save((err, post) => {
+        // REDIRECT TO THE ROOT
+        return res.redirect('/');
+      });
+    } else {
+      // UNAUTHORIZED
+      return res.status(401);
+    }
   });
 
   app.get('/posts/:id', (req, res) => {
     // LOOK UP THE POST
     Post.findById(req.params.id).lean().populate('comments').then((post) => {
-        res.render('posts-show', { post })
+        res.render('posts-show', { post }, currentUser )
     })
     .catch(err => {
         console.log(err.message);
